8b8a8f9bd4af6797e16ab04486b1142d3a59751b
Yoann <yoannlaporte08@gmail.com>
2023-05-25 16:29:33 +0200

    Ajout de la page profil et début de création de l'onglet mes informations avec le container, le titre et limage de profil

---
+++ view/profil.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../CDG/assets/dist/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@700;800&family=Roboto:ital,wght@0,500;0,700;1,400&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Document</title>
+</head>
+
+<body>
+
+<!-- Mes Informations -->
+    <div class="flex justify-center my-20">
+        <div class="w-4/5 h-56 border-2 border-purple-light rounded-lg">
+            <div class="flex mt-6">
+                <img class="w-24 h-24 mb-3 ml-6 rounded-full shadow-lg" src="" alt="Profil image"/>
+                <div class="border-b-2 border-purple-light shadow-lg py-8 px-8 ml-80">
+                    <h3 class="font-semibold text-4xl ">Mes Informations</h3>
+                </div>
+            </div>  
+        </div>
+    </div>
+
+<!-- Mes Publications -->
+
+<!-- Mes Favoris -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+</body>
+</html>
