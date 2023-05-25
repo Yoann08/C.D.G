2ef85eff8cba7b4a5d5e309286660fedcf77beac
Yoann <yoannlaporte08@gmail.com>
2023-05-25 16:24:04 +0200

    Arborescence de fichier par défaut et configuration de tailwind et ajout au projet

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <script src="/tailwind.config.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css"  rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@700;800&family=Roboto:ital,wght@0,500;0,700;1,400&display=swap" rel="stylesheet">
+    <script>
+        tailwind.config = {
+            theme: {
+                colors: 
+                {
+                    'purple-dark': '#4f2779',
+                    'purple-light': '#ad53a6',
+                    'black': '#000d32',
+                    'yellow': '#ffd700',
+                    'cyan': '#58ffd3',
+                    'background': '#fbfbfb',
+                },
+                fontFamily: 
+                {
+                    'roboto': ['Roboto', 'sans-serif'],
+                    'baloo': ['Baloo 2', 'cursive'],
+                },
+                extend: 
+                {
+                    borderRadius: 
+                    {
+                        'card-radius': '1.25rem',
+                        'button-radius': '3.125rem',
+                    }
+                },
+            },
+            plugins: [],
+        } 
+    </script>
+
+    <title>Code De Geek</title>
+</head>
+
+<body>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+</body>
+</html>
