1f175bf1cad66811cee2932c2570efcf02a8717c
Yoann <yoannlaporte08@gmail.com>
2023-05-31 14:47:46 +0200

    Ajout des modifications fait sur le main sur ma branch

---
+++ config.tailwind.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+    theme: {
+        colors: 
+        {
+            'purple-dark': '#4f2779',
+            'purple-light': '#ad53a6',
+            'black': '#000d32',
+            'yellow': '#ffd700',
+            'cyan': '#58ffd3',
+            'background': '#fbfbfb',
+        },
+        fontFamily: 
+        {
+            'roboto': ['Roboto', 'sans-serif'],
+            'baloo': ['Baloo 2', 'cursive'],
+        },
+        extend: 
+        {
+            borderRadius: 
+            {
+                'card-radius': '1.25rem',
+                'button-radius': '3.125rem',
+            }
+        },
+    },
+    plugins: [],
+} 
