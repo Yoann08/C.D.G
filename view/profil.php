152ac82149c97e84352d5a3427c8426fbcd27d81
Yoann <yoannlaporte08@gmail.com>
2023-06-01 09:01:36 +0200

    Page profil, onglet mes informations terminer ajout de tout les champs + couleur

--- view/profil.php
+++ view/profil.php
@@ -8,8 +8,9 @@ include '../config/head.php';
 <div class="flex justify-center my-20 sm">
     <div class="shadow-md shadow-purple-dark/[.7] w-4/5 pb-6 border-2 border-purple-light rounded-lg">
         <div class="flex mt-6">
+        <img class="w-24 h-24 mb-3 rounded-full shadow-lg mt-10 ml-20" src="../assets/img/codesdegeek.png" alt=""/>
             <div class="shadow-md shadow-purple-dark/[.7] py-8 px-8 mx-auto">
-                <h3 class="font-semibold text-4xl">Mes Informations</h3>
+                <h3 class="font-semibold text-4xl px-14 pt-4">Mon Profil</h3>
             </div>
         </div>  
             <div class="flex flex-row justify-start mt-12 ml-10">
@@ -26,23 +27,30 @@ include '../config/head.php';
                         </li>
                     </ul>
                 </div>
-                <div class="ml-20 ">
-                    <input class="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="default_size" type="file">
-                    <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Modifier mon email</label>
-                    <input type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                </div>
-                <div>
-                <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Modifier mon email</label>
-                    <input type="text" id="base-input" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <a href="#">Supprimer mon compte</a>
+                <div class="flex mb-16 mt-6">
+                    <div class="ml-24">
+                        <label class="block mb-2 text-sm font-medium text-purple-light dark:text-white" for="file_input">Modifier avatar</label>
+                        <input class="block w-full text-sm text-purple-light border border-purple-light rounded-lg cursor-pointer" id="file_input" type="file">
+                        <label for="email" class="mt-10 block mb-2 text-sm font-medium text-purple-light dark:text-white">Modifier email</label>
+                        <input type="email" id="email" class="bg-gray-50 border border-purple-light text-purple-light text-sm rounded-lg block w-full p-2.5">
+                    </div>
+                    <div class="ml-40">
+                    <label for="password" class="block mb-2 text-sm font-medium text-purple-light dark:text-white">Modifier mot de passe</label>
+                    <input type="password" id="password" class="mb-16 bg-gray-50 border border-purple-light text-purple-light text-sm rounded-lg block p-2.5">                   
+                    <a href="#" class="text-purple-light">Supprimer mon compte</a>
+                    </div>
                 </div>
             </div>
-        </div>    
+        </div>   
     </div> 
 
 <!-- Mes Publications -->
 
 <!-- Mes Favoris -->
+
+
+
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
     <script src="https://unpkg.com/@themesberg/flowbite@latest/dist/flowbite.bundle.js"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
