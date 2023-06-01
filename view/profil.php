0dd4c97b3f7d3b0e2ac574e0a19803d87dda16ae
Yoann <yoannlaporte08@gmail.com>
2023-06-01 14:47:25 +0200

    ajout d'un fichier style.css plus modification avatar profil+ ajout du lien du fichier css dans le head et modification du fichier css pour la page profil pour la tabulation

--- view/profil.php
+++ view/profil.php
@@ -5,10 +5,10 @@ include '../config/head.php';
 <body>
 
 <!-- Mes Informations -->
-<div class="flex justify-center my-20 sm">
+<div id="profil" class="flex justify-center my-20 sm">
     <div class="shadow-md shadow-purple-dark/[.7] w-4/5 pb-6 border-2 border-purple-light rounded-lg">
         <div class="flex mt-6">
-            <img class="w-24 h-24 mb-3 rounded-full shadow-lg mt-10 ml-20" src="../assets/img/codesdegeek.png" alt="Avatar de profil"/>
+            <img class="w-24 h-24 mb-3 rounded-full shadow-lg mt-10 ml-20" src="../assets/img/avatar.png" alt="Avatar de profil"/>
             <div class="shadow-md shadow-purple-dark/[.7] h-20 px-6 mt-10 mx-auto">
                 <h3 class="font-semibold text-4xl px-14 pt-4">Mon Profil</h3>
             </div>
@@ -17,13 +17,13 @@ include '../config/head.php';
             <div class="flex flex-row ml-6 w-auto border-b border-gray-200">
                 <ul class="flex flex-col -mb-px text-sm font-medium text-center mt-10" id="myTab" data-tabs-toggle="#myTabContent" role="tablist">
                     <li class="mr-2 " role="presentation">
-                        <button class="inline-block w-44 px-2 py-2 text-white bg-purple-light border border-purple-light flex items-center justify-between" id="informations-tab" data-tabs-target="#informations" type="button" role="tab" aria-controls="informations" aria-selected="false"><img src="../assets/img/apprenants.png">Mes informations</button>
+                        <button class="inline-block w-44 px-2 py-2 hover:text-white text-white bg-purple-light flex items-center justify-between" id="informations-tab" data-tabs-target="#informations" type="button" role="tab" aria-controls="informations" aria-selected="true"><img src="../assets/img/apprenants.png">Mes informations</button>
                     </li>
                     <li class="mr-2" role="presentation">
-                        <button class="inline-block w-44 px-2 py-2 text-white bg-purple-light border border-purple-light flex items-center justify-between" id="publications-tab" data-tabs-target="#publications" type="button" role="tab" aria-controls="publications" aria-selected="false"><img src="../assets/img/publications.png">Mes Publications</button>
+                        <button class="inline-block w-44 px-2 py-2 hover:text-white text-white bg-purple-light flex items-center justify-between" id="publications-tab" data-tabs-target="#publications" type="button" role="tab" aria-controls="publications" aria-selected="false"><img src="../assets/img/publications.png">Mes Publications</button>
                     </li>
                     <li class="mr-2" role="presentation">
-                        <button class="inline-block w-44 px-2 py-2 text-white bg-purple-light border border-purple-light flex items-center justify-between" id="favoris-tab" data-tabs-target="#favoris" type="button" role="tab" aria-controls="favoris" aria-selected="false"><img src="../assets/img/favoris.png">Mes Favoris</button>
+                        <button class="inline-block w-44 px-2 py-2 hover:text-white text-white bg-purple-light flex items-center justify-between" id="favoris-tab" data-tabs-target="#favoris" type="button" role="tab" aria-controls="favoris" aria-selected="false"><img src="../assets/img/favoris.png">Mes Favoris</button>
                     </li>
                 </ul>
             </div>
