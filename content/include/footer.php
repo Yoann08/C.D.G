fa467e23b4e7c6314c5a11c0580124e2a58047bb
barca0812 <jullion.stephane0812@gmail.com>
2023-06-01 09:45:22 +0200

    footer responsive

---
+++ content/include/footer.php
@@ -0,0 +1,44 @@
+<footer class="bg-violet text-black text-base py-4">
+    <div class="container mx-auto px-4 md:px-8 lg:px-16 xl:px-20">
+        <div class="flex flex-col md:flex-row justify-between">
+            <div class="text-center px-3">
+                <span class="font-bold">CODES DE GEEK</span>
+                <div class="flex mt-4 justify-center md:justify-start">
+                    <a href="#" class="ml-2 mr-2 text-black hover:text-purple-light">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href="#" class="ml-2 mr-2 text-black hover:text-purple-light">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="#" class="ml-2 mr-2 text-black hover:text-purple-light">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="#" class="ml-2 mr-2 text-black hover:text-purple-light">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                </div>
+                <div class="mt-2">
+                    <a href="#" class="text-black hover:text-white">CGU</a>
+                </div>
+                <div class="mt-2">
+                    <a href="#" class="text-black hover:text-white">Mentions légales</a>
+                </div>
+            </div>
+            <div class="text-center mt-4 md:mt-0">
+                <div class="font-bold">NOUS TROUVER</div>
+                <div class="mt-4">UIMM Campus Sud Ardennes</div>
+                <div class="mt-2">08000 Charleville-Mézières</div>
+                <div class="mt-2">Ouvert du lundi au vendredi</div>
+                <div>Horaires: 8h15-12h15 / 13h30-16h30</div>
+            </div>
+            <div class="text-center mt-4 md:mt-0">
+                <div class="font-bold">NOUS CONTACTER</div>
+                <img src="assets/image/courrier.png" alt="Logo" class="h-4 hidden md:block mx-auto mt-4">
+                <div class="mt-2">cdg@simplon.fr</div>
+                <img src="assets/image/phone.png" alt="Logo" class="h-4 hidden md:block mx-auto mt-2">
+                <div class="mt-2">03.24.01.23.45</div>
+            </div>
+        </div>
+    </div>
+    <div class="text-center mt-4 text-sm">&copy; 2023 CODES DE GEEK. Tous droits réservés.</div>
+</footer>
