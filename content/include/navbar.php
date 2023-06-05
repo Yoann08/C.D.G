1a83729e0353ac666173659aecb5bf18a5c3ef1d
Juliette Devillé <julietted@live.fr>
2023-06-05 09:34:01 +0200

    V10 Fin front

--- content/include/navbar.php
+++ content/include/navbar.php
@@ -1,32 +1,53 @@
 
-<div class="bg-violet text-white py-4 shadow-md shadow-purple-dark/[.7]">
-        <div class="container mx-auto flex justify-between">
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="https://flowbite.com/" class="flex items-center">
+        <img src="../../../C.D.G/assets/image/logo.png" class="h-20 hidden md:block mr-3" alt="Logo" />
+    </a>
+    <div class="gap-2 flex items-center md:order-2">
+    <button type="button" class="flex mr-4 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+        <span class="sr-only">Open user menu</span>
+        <img class="w-8 h-8 rounded-full" src="assets/image/crud.png" alt="user photo">
+    </button>
+    <button type="button" class="flex mr-4 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+        <span class="sr-only">Open user menu</span>
+        <img class="w-8 h-8 rounded-full" src="assets/image/avatar.png" alt="user photo">
+    </button>
+    <a href="#" class="px-3 py-2 hover:text-gray-300 text-black">Connexion</a>
+    <a href="#" class="px-3 py-2 hover:text-gray-300 text-black">Inscription</a>
 
-            <div class="flex items-center ml-8">
-                <img src="assets/image/logo.png" alt="Logo" class="h-20 hidden md:block">
-            </div>
-            <div class="flex items-center max-w-lg mx-auto">
+      <!-- Dropdown menu -->
+      
+      <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+        <div class="px-4 py-3">
+          <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+          <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+        </div>
+        <ul class="py-2" aria-labelledby="user-menu-button">
+          <li>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+          </li>
+          <li>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+          </li>
+          <li>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+          </li>
+          <li>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+          </li>
+        </ul>
+      </div>
+      <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+    </button>
+  </div>
+  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
+  <div class="flex items-center max-w-lg mx-auto">
                 <div class="flex items-center">
-                    <button class="text-black bg-transparent hover:bg-violet   font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" type="button" data-dropdown-toggle="dropdown">HTML <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-                        </svg></button>
-                    <!-- Dropdown menu -->
-                    <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-2 py-2" id="dropdown">
-                        <ul class="py-1" aria-labelledby="dropdown">
-                            <li>
-                                <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Cours 1</a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Cours 2</a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Cours 3</a>
-                            </li>
-                            <li>
-                                <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Cours 4</a>
-                            </li>
-                        </ul>
-                    </div>
+                    <button class="text-black bg-transparent hover:bg-violet   font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" type="button" data-dropdown-toggle="dropdown">HTML <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"></svg></button>                  
+                </div>
                     <div class="max-w-lg mx-auto">
                         <div class="flex items-center">
                             <button class="text-black bg-transparent hover:bg-violet  font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" type="button" data-dropdown-toggle="dropdown">CSS <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -78,41 +99,6 @@
                                     <button class="text-black bg-transparent hover:bg-violet  font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" type="button" data-dropdown-toggle="dropdown">PHP <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
                                         </svg></button>
-
-                                    <!-- Dropdown menu -->
-                                    <div class="hidden bg-white text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-2 py-2" id="dropdown">
-                                        <ul class="py-1" aria-labelledby="dropdown">
-                                            <li>
-                                                <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Cours 1</a>
-                                            </li>
-                                            <li>
-                                                <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Cours 2</a>
-                                            </li>
-                                            <li>
-                                                <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Cours 3</a>
-                                            </li>
-                                            <li>
-                                                <a href="#" class="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2">Cours 4</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="flex items-center mx-8">
-                <a href="#" class="mr-2 text-black hover:text-white">
-                <img class="w-8 mt-0.5" src="assets/image/crud.png"></img>
-                </a>
-                <a href="#" class="mr-2 -mt-0.5 text-black hover:text-white">
-                <img class="w-8" src="assets/image/avatar.png"></img>
-                </a>
-                <a href="#" class="px-3 py-2 hover:text-gray-300 text-black">Connexion</a>
-                <a href="#" class="px-3 py-2 hover:text-gray-300 text-black">Inscription</a>
-            </div>
-            <script src="https://unpkg.com/@themesberg/flowbite@latest/dist/flowbite.bundle.js"></script>
-        </div>
-    </div>
-    </div>
+  </div>
+  </div>
+</nav>
