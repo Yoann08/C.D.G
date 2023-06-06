51baa43876fe80bedbee1af28a3d9c1a7e572cde
Yoann <yoannlaporte08@gmail.com>
2023-06-06 09:36:54 +0200

    Ajout d'un fichier swiper.css + modification du head de l'accueil.php + include et modification des chemin dans le head.php

--- include/head.php
+++ include/head.php
@@ -4,14 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- TAILWIND -->
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
-    <link rel="icon" href="../assets/img/codesdegeek.png">
+    <script src="../../C.D.G/assets/js/config.tailwind.js"></script>
+
+    <!-- FLOWBITE -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css"  rel="stylesheet" />
+
+    <!-- FONT GOOGLE -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@700;800&family=Roboto:ital,wght@0,500;0,700;1,400&display=swap" rel="stylesheet">
-    <script src="../config.tailwind.js"></script>
-    <link rel="stylesheet" href="../../assets/style/index.css">
+
+    <!-- FONTAWESOME -->
+    <script src="https://kit.fontawesome.com/a7a859a4e2.js" crossorigin="anonymous"></script>
+
+    <!-- JAVASCRIPT   -->
+    <script src="../../C.D.G/assets/js/script.js"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../C.D.G/assets/style/style.css">
+    <link rel="stylesheet" href="../../C.D.G/assets/style/swiper.css">
+
+    <!-- ICON -->
+    <link rel="icon" href="../assets/img/codesdegeek.png">
+
     <title>Codes de geek</title>
 </head>
